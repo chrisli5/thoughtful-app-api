789e6bc74722bee24fdffc17cf4c35759b97d92d
chrisli5 <chrisli41@gmail.com>
2020-03-04 03:46:07 -0800

    Update notes table.

--- seeds/seed.notes.sql
+++ seeds/seed.notes.sql
@@ -1,6 +1,6 @@
-INSERT INTO notes (user_id, content)
+INSERT INTO notes (user_id, content, mood, time_spent)
 VALUES
-    (1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
-    (1, 'Varius quam quisque id diam.'),
-    (1, 'Id consectetur purus ut faucibus pulvinar elementum integer enim neque.'),
-    (1, 'In nulla posuere sollicitudin aliquam ultrices sagittis. Id aliquet risus feugiat in.');
+    (1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '1', 120),
+    (1, 'Varius quam quisque id diam.', '2', 123),
+    (1, 'Id consectetur purus ut faucibus pulvinar elementum integer enim neque.', '3', 300),
+    (1, 'In nulla posuere sollicitudin aliquam ultrices sagittis. Id aliquet risus feugiat in.', '4', 431);
